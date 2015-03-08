aad748b7387837fea87eae601b3271a97006c1cd
David Kudera <sakren@gmail.com>
2015-03-08 13:03:39 +0100

    add gulp task for building source

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+var coffee = require('gulp-coffee');
 
 var browserify = require('browserify');
 var coffeeify = require('coffeeify');
 
 var source = require('vinyl-source-stream');
+
+gulp.task('compile-source', function() {
+	return gulp.src('./src/**/*.coffee')
+		.pipe(coffee().on('error', gutil.log))
+		.pipe(gulp.dest('./lib/'));
+});
 
 gulp.task('compile-tests', function() {
 	var bundler = browserify({extensions: ['.coffee']})
